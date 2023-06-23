59114f148dbbff392798b23213e39d8dadc426ae
= <Pushks2015@gmail.com>
2023-06-23 23:02:49 +0530

    cors issue fix 4

--- app.js
+++ app.js
@@ -15,15 +15,14 @@ config({
 //Using middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//     optionSuccessStatus: 200,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 
 //using routes
 app.use("/api/v1/users", userRouter);
